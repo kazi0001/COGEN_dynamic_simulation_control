7730797b66103cc6c9fdd7d99e9f77c4790e15f5
Kazi Monzure Khoda <62577662+kazi0001@users.noreply.github.com>
2022-06-01 12:50:12 -0500

    Add files via upload

---
+++ Mcogen1.m
@@ -0,0 +1,15 @@
+% Binary dispersiion and film mass transfer
+% Kazi Monzure Khoda, Post-doc Researcher, Qatar University, 2014.
+
+% % odebdkq1: From odescc3: Removing mad, rhos from Params, global vars removed
+%  Using q-explicit relations, using new axial dispersion correlation
+% Using SI units, tested vs odebdspk1
+% 2: Axial dispersion given as parameters
+
+clc
+clear
+clear global
+
+% clear all
+
+close all
